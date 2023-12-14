1f3c1f1d5b98de7d15d007e7727688819ca36d93
BurgerFanMan <naumanwaqas2008@gmail.com>
2023-12-14 13:17:33 +0500

    Further cleanup

--- Assets/Scripts/Player/SniperManager.cs
+++ Assets/Scripts/Player/SniperManager.cs
@@ -8,14 +8,17 @@ using TMPro;
 public class SniperManager : ICanBeUpgraded
 {
     [Header("Main")]
-    public List<Sniper> _snipers;
     [SerializeField] GameObject _bulletPrefab;
-    public float _reloadTime;
-    public int _magazineSize;
+    [SerializeField] float _reloadTime;
+    [SerializeField] int _magazineSize;
+    [SerializeField] float _fireRate = 1f;
+
     [SerializeField] float _turnSpeed;
     [SerializeField] float _rotationClamp = 90f;
-    [SerializeField] List<Transform> _followMouse;
+    
     [SerializeField] Transform _reloadIcon;
+    [SerializeField] List<Transform> _followMouse;
+    [SerializeField] List<Sniper> _snipers;
 
     [Header("UI")]
     [SerializeField] TextMeshProUGUI _reloadNumb;
@@ -23,12 +26,17 @@ public class SniperManager : ICanBeUpgraded
     [Tooltip("Won't fire if mouse is hovering over UI of this tag")]
     [SerializeField] string _forbiddenUITag;
 
-    private Vector3 targetPos; 
-    float timeSinceFire; 
-    int magazineCount;
-    private AudioSource audioSource;
+    
+    private float timeSinceReload; 
+    private int magazineCount;  
     private bool reloadFirstFrame = true;
+    private bool readyToFire = true;
+    private float timeDone;
+
+    private Vector3 targetPos;
+
     private RayStore rayStore;
+    private AudioSource audioSource;
 
     protected override void Awake()
     {
@@ -45,51 +53,64 @@ public class SniperManager : ICanBeUpgraded
     }
     void Update()
     {
-        if (!_paused)
+        if (_paused)
+            return;
+
+        bool overUI = EventSystem.current.IsPointerOverGameObject() &&
+                      EventSystem.current.currentSelectedGameObject != null &&
+                      EventSystem.current.currentSelectedGameObject.CompareTag(_forbiddenUITag) ? true
+                      : false;
+
+        targetPos = rayStore.RayHitPoint();
+        targetPos.y = 0;
+
+
+        _reloadNumb.text = magazineCount.ToString();
+        _reloadCap.text = (_magazineSize + upgradeLevel[1]).ToString();
+
+        foreach (Transform trans in _followMouse)
         {
-            foreach(Transform tran in _followMouse)
-            {
-                tran.position = Input.mousePosition;
-            }
-            
-            _reloadNumb.text = magazineCount.ToString();
-            _reloadCap.text = (_magazineSize + upgradeLevel[1]).ToString();
+            trans.position = Input.mousePosition;
+        }
+        foreach (Sniper sniper in _snipers)
+        {
+            sniper.FacePosition(targetPos);
         }
 
         if (magazineCount <= 0)
         {
             if (reloadFirstFrame)
             {
-                _reloadIcon.gameObject.SetActive(true);
+                _reloadIcon.gameObject.SetActive(true);   
+
+                readyToFire = true;
+                timeDone = 0f;
+
                 reloadFirstFrame = false;
             }
 
-            timeSinceFire += 1 * Time.deltaTime * _timeScale;
-            _reloadIcon.localScale = new Vector3(timeSinceFire / (_reloadTime * upgradeLevel[0]), 1f, 1f);
-            
-            if(timeSinceFire >= _reloadTime * upgradeLevel[0])
+            timeSinceReload += 1 * Time.deltaTime * _timeScale;
+            _reloadIcon.localScale = new Vector3(timeSinceReload / (_reloadTime * upgradeLevel[0]), 1f, 1f);
+
+            if (timeSinceReload >= _reloadTime * upgradeLevel[0])
             {
-                magazineCount = _magazineSize + (int)upgradeLevel[1]; timeSinceFire = 0;
+                magazineCount = _magazineSize + (int)upgradeLevel[1]; timeSinceReload = 0f;
                 _reloadIcon.gameObject.SetActive(false);
 
                 reloadFirstFrame = true;
             }
         }
-
-        targetPos = rayStore.RayHitPoint();
-        targetPos.y = 0;
-
-        foreach(Sniper sniper in _snipers)
+        else if (!readyToFire)
         {
-            sniper.FacePosition(targetPos);
-        }
-        //deal with this
-        bool overUI = EventSystem.current.IsPointerOverGameObject() &&
-                      EventSystem.current.currentSelectedGameObject != null &&
-                      EventSystem.current.currentSelectedGameObject.CompareTag(_forbiddenUITag) ? true 
-                      : false;
+            timeDone += Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Mouse0) && magazineCount > 0 && !overUI && !_paused)
+            if(timeDone > 1f / (_fireRate * upgradeLevel[2]))
+            {
+                readyToFire = true;
+                timeDone = 0f;
+            }
+        }
+        else if (Input.GetKey(KeyCode.Mouse0) && !overUI)
         {
             if(audioSource != null)
                 audioSource.Play();
@@ -98,6 +119,8 @@ public class SniperManager : ICanBeUpgraded
                 if (sniper.Shoot())
                     break;
             }
+
+            readyToFire = false;
             magazineCount--;
         }
     }
