46cec21f11979ce0f2cdc97797233d1f1ad27f27
BurgerFanMan <naumanwaqas2008@gmail.com>
2024-03-05 18:01:28 +0500

    Improved main menu and camera movement

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    [SerializeField] Transform _cameraParent;
-    [SerializeField] float _speed;
+    public float speed;
+    public float targetPosWeight = 1f;
+    public CameraMode cameraMode = CameraMode.SmoothDamp;
+    public Transform cameraParent;
 
+    private Vector3 velocity = Vector3.zero;
     private Vector3 origin = new Vector3(0f, 0f, 0f);
 
     void Update()
     {
         if (!Pause.isPaused && !Input.GetKey(InputManager.GetValue("camera_lockposition")))
         {
-            Vector3 _targetPos;
-            _targetPos = RayStore.GroundedHitPoint;
-            _targetPos.y = 0;
-            Vector3 newPos = ((origin + _targetPos) / 2);
+            Vector3 targetPos;
+            targetPos = RayStore.GroundedHitPoint;
+            targetPos.y = 0;
+            Vector3 newPos = ((origin + (targetPos * targetPosWeight)) / (1f + targetPosWeight));
 
-            Vector3 movement = Vector3.MoveTowards(_cameraParent.position, newPos, _speed * Time.deltaTime);
-            _cameraParent.position = movement;
+            Vector3 movement;
+            
+            if(cameraMode == CameraMode.Lerp)
+                movement = Vector3.Lerp(transform.position, newPos, speed);
+            else if(cameraMode == CameraMode.SmoothDamp)
+                movement = Vector3.SmoothDamp(transform.position, newPos, ref velocity, 1f/speed);
+            else if(cameraMode == CameraMode.MoveTowards)
+                movement = Vector3.MoveTowards(transform.position, newPos, speed);
+            else
+                movement = newPos;
+            
+            cameraParent.position = movement;
         }
     }
+}
+
+public enum CameraMode{
+    Lerp,
+    SmoothDamp,
+    MoveTowards,
+    NoInterpolation
 }
